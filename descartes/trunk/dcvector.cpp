6829aac0c2f54e83f04a2cc6c29a2a124a2236bd
bob.burrough <bob.burrough@d3f1cc82-c306-40fe-8310-c3bf58b39332>
2016-01-17 22:29:31 +0000

    Make appropriate interfaces into descartes classes.

--- descartes/trunk/dcvector.cpp
+++ descartes/trunk/dcvector.cpp
@@ -14,24 +14,24 @@ DCVector::DCVector(LineSegment line_arg)
 	p = line_arg.B - line_arg.A;
 }
 
-mm DCVector::Magnitude()
+mm DCVector::Magnitude() const
 {
 	return sqrt(p.GetX()*p.GetX() + p.GetY()*p.GetY() + p.GetZ()*p.GetZ());
 }
 
-mm DCVector::Dot(Vector v)
+mm DCVector::Dot(DCVector v) const
 {
 	return (p.GetX() * v.p.GetX()) + (p.GetY() * v.p.GetY()) + (p.GetZ() * v.p.GetZ());
 }
 
-degrees DCVector::AngleBetween(Vector v)
+degrees DCVector::AngleBetween(DCVector v) const
 {
 	return acos( this->Dot(v) / (this->Magnitude() * v.Magnitude()) ) * (180.0f / PI);
 }
 
 DCVector DCVector::operator*(const float& s) const
 {
-	Vector temp(*this);
+	DCVector temp(*this);
 	temp.p.SetX(temp.p.GetX() * s);
     temp.p.SetY(temp.p.GetY() * s);
     temp.p.SetZ(temp.p.GetZ() * s);
@@ -48,7 +48,7 @@ DCVector DCVector::operator*=(const float& s)
 
 DCVector DCVector::operator/(const float& s) const
 {
-	Vector temp(*this);
+	DCVector temp(*this);
 	temp.p.SetX(temp.p.GetX() / s);
     temp.p.SetY(temp.p.GetY() / s);
     temp.p.SetZ(temp.p.GetZ() / s);
@@ -63,3 +63,16 @@ DCVector DCVector::operator/=(const float& s)
     return *this;
 }
 
+LRO DCVector::LeftRightOrOn(Point theta) const
+{
+    // (Bx - Ax) * (Cy - Ay) - (By - Ay) * (Cx - Ax)
+    // (Bx - 0) * (Cy - 0) - (By - 0) * (Cx - 0)
+    // Bx * Cy - By * Cx
+    float val = p.GetX() * theta.GetY() - p.GetY() * theta.GetX();
+    if (FloatsEqual(val, 0.0f))
+        return ON;
+    else if (val < 0.0f)
+        return RIGHT;
+    else
+        return LEFT;
+}
