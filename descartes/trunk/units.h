17527a5bc1208a95f8589751fdb6ff490e229fa4
bob.burrough <bob.burrough@d3f1cc82-c306-40fe-8310-c3bf58b39332>
2016-01-18 03:38:40 +0000

    Add documentation.

--- descartes/trunk/units.h
+++ descartes/trunk/units.h
@@ -1,24 +1,45 @@
 #ifndef _UNITS_H_
 #define _UNITS_H_
 
+/* 12345678911234567892123456789312345678941234567895123456789612345678971234567898 */
 #include <cfloat>
 #include <cmath>
 
 using namespace std;
 
+/*
+    PI out to silly precision.  Relevant reading:
+    http://blogs.scientificamerican.com/observations/how-much-pi-do-you-need/
+*/
 #define PI 3.1415926535897932384626433832f
 #define EPSILON FLT_EPSILON
 
+
+/*
+    Compare floats a and b.  Return true if they are nearer to each other than the
+    tolerance.  Otherwise, return false.
+*/
 static bool FloatsWithinTolerance(float a, float b, float tolerance)
 {
     return (a / b) > 1.0f - tolerance && (a / b) < 1.0f + tolerance;
 }
 
+
+/*
+    Compare floats a and b.  Return true if they are within the machine's epsilon
+    (FLT_EPSILON) ofeach other.  Note: this is a problematic comparison.  Relevant
+    reading: http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
+*/
 static bool FloatsEqual(float a, float b)
 {
     return fabs(a - b) < EPSILON;
 }
 
+
+/*
+    Windows (in its abject laziness) doesn't provide isnan().  Therefore, we
+    provide it (and relatives) here.
+*/
 #if WIN32
 #ifndef NAN
 static const unsigned long __nan[2] = { 0xffffffff, 0x7fffffff };
@@ -38,6 +59,11 @@ static const unsigned long __nan[2] = { 0xffffffff, 0x7fffffff };
 #endif
 #endif
 
+
+/*
+    isan() simply returns the inverse of isnan.  It's provided to make
+    isnan() comparison logic more clear.
+*/
 #ifndef isan
 #define isan(x) (!isnan(x))
 #endif
