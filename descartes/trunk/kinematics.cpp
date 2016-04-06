9fc6ce92903019212719f65ece522401dd3c650e
bob.burrough <bob.burrough@d3f1cc82-c306-40fe-8310-c3bf58b39332>
2016-04-06 21:35:47 +0000

    These modifications were made over the course of adding support for momentum-conservation in the simulator. It meant that (virtually all of) our prior kinematics functions were inappropriate because they assumed acceleration from (or to) a dead stop.  There's also a bit of data type cleanup here.

--- descartes/trunk/kinematics.cpp
+++ descartes/trunk/kinematics.cpp
@@ -7,6 +7,8 @@ Kinematic Equations
 
 distance = initial_velocity * time + 1/2 * acceleration * time^2
 
+time = sqrt(2 * distance / acceleration) 
+
 final_velocity^2 = initial_velocity^2 + 2 * acceleration * distance
 
 final_velocity = initial_velocity + acceleration * time
@@ -15,28 +17,39 @@ distance = (initial_velocity + final_velocity) / 2 * time
 */
 
 
-
 // time to max velocity from a dead stop
-seconds TimeToMaxVelocity(mmM max_velocity, mmS2 acceleration_arg)
+seconds TimeToVelocity(mmM final_velocity, mmS2 acceleration_arg)
 {
-    return (max_velocity / 60.0f) / acceleration_arg;
+    return final_velocity / 60.0f / acceleration_arg;
 }
+
 
 
 // time to max velocity starting from a pre-existing velocity
-seconds TimeToMaxVelocity(mmM initial_velocity, mmM max_velocity, mmS2 acceleration_arg)
+seconds TimeToVelocity(mmM initial_velocity, mmM final_velocity, mmS2 acceleration_arg)
 {
-    return (max_velocity - initial_velocity) / acceleration_arg;
+    return (final_velocity - initial_velocity) / 60.0f / acceleration_arg;
 }
 
 
-
-
-mm DistanceToMaxVelocity(mmM max_velocity, mmS2 acceleration_arg)
+mm DistanceToVelocity(mmM final_velocity, mmS2 acceleration_arg)
 {
     //x = 1 / 2 * acceleration * tmax ^ 2
-    seconds tmax = TimeToMaxVelocity(max_velocity, acceleration_arg);
+    seconds tmax = TimeToVelocity(final_velocity, acceleration_arg);
     return 0.5f * acceleration_arg * (tmax * tmax);
+}
+
+
+mm DistanceToVelocity(mmM initial_velocity, mmM final_velocity, mmS2 acceleration_arg)
+{
+    if (FloatsEqual(initial_velocity, final_velocity)) return 0.0f;
+    if (FloatsEqual(acceleration_arg, 0.0f)) throw(exception("impossible to decelerate from initial_velocity to final velocity because acceleration is zero"));
+    if (acceleration_arg <= 0.0f && final_velocity >= initial_velocity) throw(exception("impossible to decelerate from initial_velocity to final velocity because final_velocity >= initial_velocity"));
+    if (initial_velocity < 0.0f || final_velocity < 0.0f) throw(exception("velocity can't be negative"));
+
+    //x = 1 / 2 * acceleration * tmax ^ 2
+    seconds tmax = TimeToVelocity(initial_velocity, final_velocity, acceleration_arg);
+    return  initial_velocity * tmax + 0.5f * acceleration_arg * (tmax * tmax);
 }
 
 
@@ -46,11 +59,67 @@ seconds TimeToAccelerateOverDistance(mm distance, mmS2 acceleration_arg)
 }
 
 
-
-seconds DistanceGivenAccelerationOverTime(seconds time, mmS2 acceleration_arg)
+seconds TimeToAccelerateOverDistance2(mmM initial_velocity, mm distance, mmS2 acceleration_arg)
 {
+    return initial_velocity / distance + sqrtf(2.0f * distance / acceleration_arg);
+}
+
+
+
+// time to max velocity starting from a pre-existing velocity
+seconds TimeToAccelerateOverDistance(mmM initial_velocity, mm distance, mmS2 acceleration_arg)
+{
+    if (FloatsEqual(0.0f, distance))
+        return distance;
+    if (acceleration_arg <= 0.0f && initial_velocity <= 0.0f)
+        return NAN; // you can't decelerate from zero
+
+    mmS velocity_S = initial_velocity / 60.0f;
+
+    bool test = false;
+    float up = (-velocity_S + sqrt(velocity_S*velocity_S + 2.0f * acceleration_arg * distance)) / acceleration_arg;
+    float down = (-velocity_S - sqrt(velocity_S*velocity_S + 2.0f * acceleration_arg * distance)) / acceleration_arg;
+    if (isnan(up))
+        test = true;
+
+    // standard quadratic solver
+    return (-velocity_S + sqrt(velocity_S*velocity_S + 2.0f * acceleration_arg * distance)) / acceleration_arg;
+
+    // Citardauq quadratic solver
+    //return (2.0f * -distance) / (-velocity_S - sqrt(velocity_S*velocity_S + 2.0f * acceleration_arg * distance));
+}
+
+
+// from dead stop
+mm DistanceGivenAccelerationOverTime(seconds time, mmS2 acceleration_arg)
+{
+    if (FloatsEqual(time, 0.0f)) return 0.0f;
+    if (acceleration_arg <= 0.0f) return 0.0f;
+    if (time < 0.0f) throw(exception("can't decelerate over negative time"));
     return  0.5f * acceleration_arg * (time * time);
 }
+
+
+// from initial_velocity
+mm DistanceGivenAccelerationOverTime(mmM initial_velocity, seconds time, mmS2 acceleration_arg)
+{
+    if (FloatsEqual(time, 0.0f)) return 0.0f;
+    if (acceleration_arg < 0.0f && FloatsEqual(initial_velocity, 0.0f)) return 0.0f;
+    if (initial_velocity < 0.0f) throw(exception("velocity can't be negative"));
+    if (time < 0.0f) throw(exception("can't decelerate over negative time"));
+
+    return initial_velocity * time / 60.0f + 0.5f * acceleration_arg * (time * time);
+}
+
+// Vf^2 = Vi^2 + 2a * (Xf - Xi)
+// Xf - Xi = Vi^2 / Vf^2 + 2a / Vf^2
+
+
+mm DistanceGivenVelocitiesOverTime(mmM initial_velocity, mmM final_velocity, seconds time)
+{
+    return (initial_velocity + final_velocity) / 2 * time;
+}
+
 
 
 /*
@@ -59,6 +128,19 @@ seconds TimeToAccelerateOverDistance(mm distance)
 return TimeToAccelerateOverDistance(distance, acceleration);
 }
 */
+
+
+// from a dead stop
+mmM VelocityGivenAccelerationOverDistance(mm distance, mmS2 acceleration_arg)
+{
+    return  0.5f * acceleration_arg * (distance * distance);
+}
+
+
+mmM VelocityGivenAccelerationOverDistance(mmM initial_velocity, mm distance, mmS2 acceleration_arg)
+{
+    return sqrt((initial_velocity / 60.0f)*(initial_velocity / 60.0f) + 2 * acceleration_arg * distance) * 60.0f;
+}
 
 
 seconds AxisMoveDuration(mm distance, mmM max_feed_rate, mmS2 acceleration_arg)
@@ -81,7 +163,7 @@ seconds AxisMoveDuration(mm distance, mmM max_feed_rate, mmS2 acceleration_arg)
     // then we need to use the dum accel up to the distance at max velocity,
     // then add in the simple velocity * distance for the remaining distance
 
-    mm max_velocity_achieved_at_distance = DistanceToMaxVelocity(max_feed_rate, acceleration_arg);
+    mm max_velocity_achieved_at_distance = DistanceToVelocity(max_feed_rate, acceleration_arg);
 
     seconds max_velocity_component = 0.0f;
     if (max_velocity_achieved_at_distance < distance)
